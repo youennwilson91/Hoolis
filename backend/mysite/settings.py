972aa530f659a85874e2fe1c9dd63be6cf16fcfb
Name <youson91@hotmail.fr>
2025-06-06 16:48:46 +0200

    Add CORS middleware fallback to force headers

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'mysite.cors_middleware.ForceCorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
