5279d50fbaf07fbd5726621076e7832efcafce1c
Name <youson91@hotmail.fr>
2025-06-07 14:50:28 +0200

    fix data

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
