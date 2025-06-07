ddb7934a46c5c315725de70346205595d90c4f17
Name <youson91@hotmail.fr>
2025-06-07 14:43:26 +0200

    fix

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
