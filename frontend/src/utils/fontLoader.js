0fbe10130dc4dd3324380c91b4ee435a5426b48a
Name <youson91@hotmail.fr>
2025-06-25 00:40:19 +0200

    modification de la police d'écriture

--- frontend/src/utils/fontLoader.js
+++ frontend/src/utils/fontLoader.js
@@ -1,19 +1,10 @@
 // Font loader utility
-const WebFontLoader = {
-  loadFonts: () => {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://fonts.googleapis.com/css2?family=Agdasima:wght@400;600;700&display=swap';
-    document.head.appendChild(link);
-    
-    // Force a redraw on fonts loaded
-    link.onload = () => {
-      document.body.style.visibility = 'hidden';
-      setTimeout(() => {
-        document.body.style.visibility = '';
-      }, 50);
-    };
-  }
-};
-
-export default WebFontLoader; 
+export const loadFont = () => {
+  const link = document.createElement('link');
+  link.rel = 'preload';
+  link.href = '/champagne-limousines-bold.ttf';
+  link.as = 'font';
+  link.type = 'font/truetype';
+  link.crossOrigin = 'anonymous';
+  document.head.appendChild(link);
+}; 
